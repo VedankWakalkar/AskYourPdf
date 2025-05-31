38ff95a197393d26fb8f9c8c6c02c647f693cefb
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2025-05-31 23:01:23 +0530

    Added Chat enpoint

--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ import multer from "multer";
 import { Queue } from 'bullmq';
 import { HOST } from "./config/env.js";
 import { BULLMQ_PORT } from "./config/env.js";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { QdrantVectorStore } from '@langchain/qdrant';
+import { GOOGLE_API_KEY } from "./config/env.js";
+import { QDRANT_URL } from "./config/env.js";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI=new GoogleGenerativeAI(GOOGLE_API_KEY)
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 const app = express();
 app.use(cors())
@@ -13,6 +21,7 @@ const queue=new Queue('file-upload',{connection:{
     host:HOST,
     port:BULLMQ_PORT
 }})
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -40,6 +49,46 @@ app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
     }
   );
   return res.json({ message: 'uploaded' });
+});
+
+app.get('/chat', async (req, res) => {
+  const userQuery = 'What is LongRAG';
+
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: GOOGLE_API_KEY,
+  });
+
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: QDRANT_URL,
+      collectionName: 'langchainjs-testing',
+    }
+  );
+
+  const retriever = vectorStore.asRetriever({ k: 2 });
+  const result = await retriever.invoke(userQuery);
+
+  // Embed context directly into user's query
+  const CONTEXT_PROMPT = `
+You are an AI assistant. Use the following context from a PDF to answer the user's question.
+
+Context:
+${JSON.stringify(result, null, 2)}
+
+Question: ${userQuery}
+`;
+
+  const chat = model.startChat(); // no initial history required
+
+  const chatResult = await chat.sendMessage(CONTEXT_PROMPT);
+  const response = await chatResult.response;
+  const message = response.text();
+
+  return res.json({
+    message,
+    docs: result,
+  });
 });
 
 app.listen(8080,()=>{
